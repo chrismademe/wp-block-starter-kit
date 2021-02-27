2663e8aac0b7d4683986d307f698b064f8da75b4
Chris Galbraith <chris@resknow.co.uk>
2021-02-27 15:02:00 +0000

    Setup Block grid

--- lib/assets.php
+++ lib/assets.php
@@ -3,7 +3,7 @@
 add_action( 'wp_enqueue_scripts', function() {
 
     // CSS
-    wp_enqueue_style( 'main', get_template_directory_uri() . '/assets/css/main.css' );
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
 
     // Javascript
 
---
+++ template-parts/content.php
@@ -0,0 +1,7 @@
+<div class="block-area">
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+
+    <div class="alignwide" style="background: red; padding: 10px">Align Wide</div>
+    <div class="alignfull" style="background: red; padding: 10px">Align Full</div>
+</div>
